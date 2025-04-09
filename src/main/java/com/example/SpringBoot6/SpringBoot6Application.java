d67b7adb9db1c7ee9a42d2afda19c17f9b37c29c
rickypr2836 <carlos.perez@cua.uam.mx>
2025-04-09 11:17:29 -0600

    firts commit

---
+++ src/main/java/com/example/SpringBoot6/SpringBoot6Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot6/SpringBoot6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
